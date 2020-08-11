cc59bbfc172d42b78d51ff029643c8840f2ac073
Carlos Daniel <killer.dk20@gmail.com>
2020-08-11 01:17:33 -0300

    Finally

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,100 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+
+import * as S from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <div />;
-}
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+  const [error, setInputError] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+  return (
+    <>
+      <img src={logo} alt="github_explorer" />
+      <S.Title>Explore repositórios no Github.</S.Title>
+      <S.Form hasError={!!error} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </S.Form>
+      {error && <S.Error>{error}</S.Error>}
+
+      <S.Repositories>
+        {repositories.map(repository => (
+          <Link
+            to={`/repository/${repository.full_name}`}
+            key={repository.full_name}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight />
+          </Link>
+        ))}
+      </S.Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,9 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-// import { Container } from './styles';
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+
+import * as S from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  title: string;
+  id: number;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
 
 const Repository: React.FC = () => {
-  return <h1>Olá Mundo!</h1>;
-}
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      const [repositoryResponse, issuesResponse] = await Promise.all([
+        api.get(`repos/${params.repository}`),
+        api.get(`repos/${params.repository}/issues`),
+      ]);
+      setRepository(repositoryResponse.data);
+      setIssues(issuesResponse.data);
+    }
+
+    loadData();
+  }, [params.repository]);
+
+  return (
+    <>
+      <S.Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </S.Header>
+
+      {repository && (
+        <S.RepositoryInfo>
+          <header>
+            <img src={repository.owner.avatar_url} alt="AC/DC" />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues</span>
+            </li>
+          </ul>
+        </S.RepositoryInfo>
+      )}
+
+      <S.Issues>
+        {issues.map(issue => (
+          <a
+            href={issue.html_url}
+            rel="noopener noreferrer"
+            target="_blank"
+            key={issue.id}
+          >
+            <div>
+              <strong>{issue.user.login}</strong>
+              <p>{issue.title}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </S.Issues>
+    </>
+  );
+};
 
 export default Repository;
